e14f61739e42cfcb7b0528c5dda25f859e07aef4
Роман Морозов <stynol@mail.ru>
2020-11-05 19:22:26 +1000

    Динамический массив

---
+++ DinamMasiv/ConsoleApp1/DynamicArray.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Masivdinam
+
+{
+    public class DynamicArray
+    {
+        private int[] items;
+
+        private int size = 0;
+
+        public DynamicArray(int length)
+        {
+            items = new int[length];
+        }
+
+        public DynamicArray()
+        {
+            items = new int[0];
+        }
+
+        public string Print()
+        {
+            string result = "";
+            for (int i = 0; i < size; i++)
+            {
+                result += items[i] + " ";
+            }
+
+            return result;
+        }
+
+        public int GetCount()
+        {
+            return size;
+        }
+        public int Get(int index)
+        {
+            if (index >= size || index < 0)
+            {
+                throw new Exception("Индекс находится за пределами массива.");
+            }
+
+            return items[index];
+        }
+        public int Find(int key)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (items[i] == key)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ DinamMasiv/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace Masivdinam
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try { 
+            var array = new DynamicArray(6);
+
+            var count = array.GetCount(); // Получить количество элементов 
+            Console.WriteLine(count); // выводит 0
+
+            //var item = array.Get(3); 
+
+            var index = array.Find(7); // Получить индекс элемента, значение которого равно 7
+
+            Console.WriteLine(index); // выводит -1
+
+            index = array.Find(1); // Получить индекс элемента, равный 1
+            Console.WriteLine(index); // выводит -1
+
+            var printedArray = array.Print(); // Распечатать массив
+            Console.WriteLine(printedArray); // выводит пустую строку
+            }
+            catch { }
+            Console.ReadKey();
+        }
+
+        private static int Result(int countOne, int countOneMax)
+        {
+            if (countOne > countOneMax)
+            {
+                countOneMax = countOne;
+
+            }
+
+            return countOneMax;
+        }
+    
+    }
+}
